3125a46b644cc791a9ee24ff2b280c5d719b41b7
Mr.Ge <gdb617162726@gmail.com>
2025-09-08 14:27:33 +0800

    fix

--- analytics-service/app/api/v1/api.py
+++ analytics-service/app/api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import accounts, proxies, tasks, metrics, results, jobs, activities
+from app.api.v1.endpoints import accounts, proxies, tasks, activities
 
 api_router = APIRouter()
 
@@ -8,7 +8,4 @@ api_router = APIRouter()
 api_router.include_router(accounts.router, prefix="/analytics/accounts", tags=["Account Analytics"])
 api_router.include_router(proxies.router, prefix="/analytics/proxies", tags=["Proxy Analytics"])
 api_router.include_router(tasks.router, prefix="/analytics/tasks", tags=["Task Analytics"])
-api_router.include_router(results.router, prefix="/analytics/results", tags=["Result Analytics"])
-api_router.include_router(metrics.router, prefix="/analytics/metrics", tags=["Metrics Management"])
-api_router.include_router(jobs.router, prefix="/analytics/jobs", tags=["Jobs Management"])
 api_router.include_router(activities.router, prefix="/analytics/activities", tags=["Activities"])
